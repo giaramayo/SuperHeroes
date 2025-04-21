2247717e53ac87990ab863422de2e24ac89d0abb
garamayo <gisella.aramayo@besysoft.com>
2025-04-21 12:43:39 -0300

    Modificacion en la busqueda de heroes

--- src/app/pages/hero-consult/hero-consult.component.ts
+++ src/app/pages/hero-consult/hero-consult.component.ts
@@ -1,37 +1,46 @@
-import { Component, inject, linkedSignal, effect } from '@angular/core';
-import { HeroSearchInputComponent } from "../../hero/components/hero-search-input/hero-search-input.component";
+import { Component, inject, signal } from '@angular/core';
 import { HeroListComponent } from "../../hero/components/hero-list/hero-list.component";
-import { map, of } from 'rxjs';
 import { HeroService } from '../../hero/services/hero.service';
-import { ActivatedRoute, Router } from '@angular/router';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-hero-consult',
-  imports: [HeroSearchInputComponent, HeroListComponent, CommonModule],
+  imports: [HeroListComponent, CommonModule],
   templateUrl: './hero-consult.component.html',
   styleUrl: './hero-consult.component.css'
 })
 export class HeroConsultComponent {
 
-  heroService = inject(HeroService);
-  activatedRoute = inject(ActivatedRoute);
+  private heroService = inject(HeroService);
 
-  // Retrieve query param (e.g., from URL) to initialize the search
-  queryParam = this.activatedRoute.snapshot.queryParamMap.get('query') ?? '';
+  idValue = signal('');
+  nameValue = signal('');
+  query = signal<{ id?: string; name?: string }>({});
 
-  // Define a signal for the query parameter
-  query = linkedSignal(() => this.queryParam);
-
-  // Define a resource to fetch hero data based on the search query or retrieve all heroes if no query
   heroResource = rxResource({
-    request: () => ({ query: this.query() }),
+    request: () => this.query(),
     loader: ({ request }) => {
-      if (!request.query) {
-        return this.heroService.getAllHeroes();  // Load all heroes if no query is provided
+      const { id, name } = request;
+      if (!id && !name) {
+        return this.heroService.getAllHeroes();
       }
-      return this.heroService.searchHeroes(request.query);  // Otherwise, search by query
+      return this.heroService.searchHeroes(id, name);
     },
   });
+
+  emitSearch() {
+    this.query.set({
+      id: this.idValue().trim() || undefined,
+      name: this.nameValue().trim() || undefined
+    });
+    this.heroResource.reload();
+  }
+
+  deleteSearch() {
+    this.idValue.set('');
+    this.nameValue.set('');
+    this.query.set({});
+    this.heroResource.reload();
+  }
 }
