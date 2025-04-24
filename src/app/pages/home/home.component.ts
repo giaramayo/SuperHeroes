261e4903fcbf217d240074eada770f5c77995aa2
garamayo <gisella.aramayo@besysoft.com>
2025-04-24 18:16:41 -0300

    Se modifica componente de home y error

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -5,6 +5,12 @@ import { HeroPageComponent } from "../../hero/components/hero-page/hero-page.com
 @Component({
   selector: 'app-home',
   imports: [HeroPageComponent],
-  templateUrl: './home.component.html'
+  template: `
+    <app-hero-page
+      [urlImage]="'https://images.unsplash.com/photo-1611604548018-d56bbd85d681?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'"
+      [title]="'Héroe!'"
+      [mensaje]="'Bienvenido a esta aplicación donde podras visualizar, crear y editar superhéroes de manera dinámica.'"
+      [button]="'Consultar'" [urlButton]="'/heroes'" />
+  `
 })
 export class HomeComponent { }
